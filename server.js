545913a3845dbe88b0261c9558fddc1b44388656
JulienToons <32437624+JulienToons@users.noreply.github.com>
2020-06-21 16:48:49 -0700

    added pic and uploaded charles' server.js file

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
-test test
+
+const express = require('express');
+const app = express();
+port = 3000;
+app.get('/', function (req, res) { res.sendFile('./index.html', { root: dirname }); });
+app.get('/', function (req, res) { res.sendFile('./index2.html', { root: dirname }); });
+app.use('/public', express.static('public'));
+app.listen(process.env.port || 3000);
+console.log('Runnning on Port 3000');
