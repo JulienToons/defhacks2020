a5a55e2cdec1236e38506087b3ed574e07830bd1
aayus <aayush01@live.com>
2020-06-21 17:39:49 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
+var path = require("path");
+var __dirname = path.resolve();
 port = 3000;
-app.get('/', function (req, res) { res.sendFile('./index.html', { root: dirname }); });
-app.get('/', function (req, res) { res.sendFile('./index2.html', { root: dirname }); });
+app.get('/', function (req, res) { res.sendFile('./index.html', { root: __dirname }); });
+app.get('/', function (req, res) { res.sendFile('./index2.html', { root: __dirname }); });
 app.use('/public', express.static('public'));
 app.listen(process.env.port || 3000);
 console.log('Runnning on Port 3000');
